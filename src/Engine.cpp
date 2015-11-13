0aa9a1dc0a71c30822925c7ce1b997a17425e07d
Arska <aarni.halinen@aalto.fi>
2015-11-13 21:01:30 +0200

    fixing merge

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -1,23 +1,28 @@
 #include "engine.hpp"
+#include "Engine.h"
 
 #define COOLDOWN 1
+
+/* Main function in the Engine namespace. Calls all functions handling position calculation and handles drawing new frame
+Parameters:
+- window: The window game is drawn on
+- players: vector consisting of every player and AI 
+- objects: vector of all objects on map
+- map: Game map
+- userinput: pair consisting of userinput given by player (2nd true if input given)
+*/
 
 void Engine::update(sf::renderWindow* window, std::vector<Player> players, std::vector<Object> objects, Map map, std::pair<inputtype, bool> userinput)
 {
 	for (auto it = player.begin(); it != player.end(); it++)
 	{
-		playerInput(it,)
+		playerInput(it, userinput);
 	}
 	for (auto it = objects.begin(); it != objects.end(); it++)
 	{
-		switch (it.getType())
-		{
-		case projectile:
-			calculatePosProjectile(it);
-		case Vehicle:
-			calculatePosObject(it);
-		}
+		calculatePosObject(it);
 	}
+	draw(window, objects, map);
 }
 
 /* Handles userinput before moving the object*/
@@ -39,14 +44,16 @@ void Engine::playerInput(Player player, std::pair<inputtype, bool> userinput))
 			// Open menu? What other inputs we have?
 		}
 	}
-	//calculatePosObject(player);
+	//calculatePosObject(player);		// If player vehicles are in objects, own calcualtion is not needed
 }
 
 void Engine::calculatePosObject(Object object)
 {
-	if (object.speed != 0)
+	float speed = object.getSpeed();
+	if (speed > 0)
 	{
-		object.move();
+		sf::vector2f velocity_vector(speed, direction);
+		object.move(velocity_vector);
 	}
 }
 /*
@@ -55,27 +62,24 @@ void Engine::calculatePosProjectile(Object projectile)
 	while (projectile.getLocation())
 }
 */
-void Engine::draw_map()
+void Engine::draw_map(sf::renderWindow* window, Map map)
+{
+	for (unsigned int x = 0; x < map.size().first(); x++)
+		for (unsigned int y = 0; y < map.size().second(); y++)
+		{
+
+		}
+}
+
+void Engine::draw_objects(sf::renderWindow* window)
 {
 
 }
 
-void Engine::draw_objects()
-{
-
-}
-
-void Engine::draw_vehicles()
-{
-
-}
-
-void Engine::draw(sf::renderWindow* window)
+void Engine::draw(sf::renderWindow* window, std::vector<Object> objects, Map map)
 {
 	window.clear(sf::Color::Black);			// Clear previous frame
-	/*Draw current frame: base first (blocks), objects, players last*/
-	window.draw_map();
-	window.draw_objects();
-	window.draw_vehicles();
+	draw_map(window, map);
+	draw_objects(window, objects);
 	window.display();						// Update drawings
 }
