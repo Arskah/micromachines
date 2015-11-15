6833bfd25c50e4beeadb34aa85a5b95eabbfe9ed
Arska <aarni.halinen@aalto.fi>
2015-11-15 17:32:16 +0200

    name change

--- src/Engine.h
+++ src/Engine.h
@@ -1,6 +1,12 @@
 #pragma once
 
-class Engine
+namespace Engine
 {
-
+	void update(sf::renderWindow* window, std::vector<Player> players, std::vector<Object> objects, Map map, std::pair<inputtype, bool> userinput);
+	void playerInput(std::vector<Player> players, std::pair<inputtype, bool> userinput));
+	void calculatePosObjects(std::vector<Object> objects);
+	//void calculatePosProjectile();
+	void draw_map(sf::renderWindow* window);
+	void draw_objects(sf::renderWindow* window);
+	void draw(sf::renderWindow* window);
 };
