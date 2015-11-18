dc297b68d2e0e167641b0c3c52856c81c8cab4f2
Arska <aarni.halinen@aalto.fi>
2015-11-18 11:01:14 +0200

    Menu class development start

--- src/Menu.h
+++ src/Menu.h
@@ -1,6 +1,83 @@
 #pragma once
+#include <SFML/Graphics.hpp>
 
 class Menu
 {
+public:
+	Menu(sf::RenderWindow& window);
+	~Menu() {}
 
+protected:
+	void eventLoop(sf::RenderWindow& window);
+	void draw(sf::RenderWindow& window);
+
+	void moveUp();
+	void moveDown();
+	virtual void select() = 0;
+
+	int getAmountItems() const;
+	sf::Text getSelected() const;
+
+	int selectedItem = 0;
+	int width;
+	int height;
+private:
+	sf::Image* backgroundImage;
+	sf::Font font;
+	std::vector<std::pair<sf::Text, std::string>> items;
+};
+
+class MainMenu : public Menu
+{
+	MainMenu(sf::RenderWindow& window);
+protected:
+	virtual void select();
+private:
+	sf::Image* backgroundImage;
+	//sf::Font font = ;
+	std::vector<std::pair<sf::Text, std::string>> items;
+};
+
+class SettingsMenu : public Menu
+{
+	SettingsMenu(sf::RenderWindow& window);
+protected:
+	virtual void select();
+private:
+	sf::Image* backgroundImage;
+	//sf::Font font = ;
+	std::vector<std::pair<sf::Text, std::string>> items;
+};
+
+class SinglePlayerMenu : public Menu
+{
+	SinglePlayerMenu(sf::RenderWindow& window);
+protected:
+	virtual void select();
+private:
+	sf::Image* backgroundImage;
+	//sf::Font font = ;
+	std::vector<std::pair<sf::Text, std::string>> items;
+};
+
+class MultiPlayerMenu : public Menu
+{
+	MultiPlayerMenu(sf::RenderWindow& window);
+protected:
+	virtual void select();
+private:
+	sf::Image* backgroundImage;
+	//sf::Font font = ;
+	std::vector<std::pair<sf::Text, std::string>> items;
+};
+
+class PauseMenu : public Menu
+{
+	PauseMenu(sf::RenderWindow& window);
+protected:
+	virtual void select();
+private:
+	sf::Image* backgroundImage;
+	//sf::Font font = ;
+	std::vector<std::pair<sf::Text, std::string>> items;
 };
