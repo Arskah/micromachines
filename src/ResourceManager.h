9066c2deb39411a47916cdfa24e0b18f6a1a4643
Okko <okko.jarvinen@aalto.fi>
2015-12-13 17:15:32 +0200

    Added comments to resourcemanager header.

--- src/ResourceManager.h
+++ src/ResourceManager.h
@@ -17,8 +17,11 @@ public:
 	*/
 	ResourceManager();
 
+	// Returns a pointer to the objecttextures map
 	std::map<Config::ObjectType, sf::Texture> * getObjectTextures();
+	// Returns a pointer to the blocktextures map
 	std::map<Config::BlockType, sf::Image> * getBlockTextures();
+	// Finds a sound corresponding to the name and plays it. For example: resourcemanager.playSound('collision')
 	void playSound(std::string name);
 
 private:
@@ -34,11 +37,18 @@ private:
 	*/
 	void loadBlockTextures();
 
+	/*
+	Loading the sound files into the soundbuffers map.
+	The sound file filepaths are defined in the sounds.txt -file.
+	*/
 	void loadSoundBuffers();
 
-
+	// Map containing the object textures connected to their respective ObjectTypes.
 	std::map<Config::ObjectType, sf::Texture> objecttextures;
+	// Map containing the block textures connected to their respective BlockTypes.
 	std::map<Config::BlockType, sf::Image> blocktextures;
+	// Sound files loaded into soundbuffers. The key is a string that can be found in the sounds.txt
 	std::map<std::string, sf::SoundBuffer> soundbuffers;
+	// Active sound objects.
 	std::vector<sf::Sound> sounds;
 };
